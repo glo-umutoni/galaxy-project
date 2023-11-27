ee09f4f678f4a1245e1efa80ab609563ca25a86e
Helen <hzhao@g.harvard.edu>
2023-11-26 20:11:57 -0800

    feat : data_extraction module

--- app/data_extraction.py
+++ app/data_extraction.py
@@ -12,15 +12,6 @@ class Data:
         self.data = query_result.to_pandas()
 
     def extract_from_constraints(self, constraints:dict):
-        # constrains = {"columns":["objid", "ra", "u"],
-        #               "database" : ["PhotoObj"],
-        #               "constrain" : [("u", [0, 19.6])],
-        #             }
-        # query = "SELECT TOP 10 p.objid,p.ra,p.dec,p.u,p.g,p.r,p.i,p.z, \
-        #     p.run, p.rerun, p.camcol, p.field, s.specobjid, s.class, s.z\
-        #      as redshift, s.plate, s.mjd, s.fiberid FROM PhotoObj AS p JOIN\
-        #      SpecObj AS s ON s.bestobjid = p.objid WHERE p.u BETWEEN 0 AND \
-        #     19.6 AND g BETWEEN 0 AND 20"
         if len(constraints["database"]) > 1:
             raise ValueError("try again. do better. only one.... smh")
         
@@ -33,16 +24,11 @@ class Data:
             col_name = col[0]
 
             if lower == upper:
-                if (type(lower) == str): 
-                    constrain_list.append(f"({col_name} = \'{lower}\')")
-                else: 
-                    constrain_list.append(f"({col_name} = {lower})")
-            elif lower is None:
-                constrain_list.append(f"({col_name} < {upper})")
-            elif upper is None:
-                constrain_list.append(f"({col_name} > {lower})")
-            else:
-                constrain_list.append(f"({col_name} BETWEEN {lower} AND {upper})")
+                if (type(lower) == str): constrain_list.append(f"({col_name} = \'{lower}\')")
+                else: constrain_list.append(f"({col_name} = {lower})")
+            elif lower is None: constrain_list.append(f"({col_name} < {upper})")
+            elif upper is None: constrain_list.append(f"({col_name} > {lower})")
+            else: constrain_list.append(f"({col_name} BETWEEN {lower} AND {upper})")
 
         print(constrain_list)
         query_constrain = f"WHERE {' AND '.join(constrain_list)}"
@@ -66,8 +52,3 @@ class Data:
         left = self.data
         right = new_data.data
         self.data = left.merge(right, how="inner", on=on_column)
-
-# if __name__ == "__main__":
-#     data_test = Data()
-
-constrains = {"columns":["objid", "ra", "u"],"database" : ["PhotoObj"], "constrain" : [("u", [0, 19.6])]}
