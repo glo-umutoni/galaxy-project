4b9b71deb753cdde453a4c97e53bfce13b7c9e52
Katherine Hunter <katherinehunter@g.harvard.edu>
2023-12-09 10:55:51 -0500

    test: modify tests to use spectral data

--- test/test_classification.py
+++ test/test_classification.py
@@ -3,11 +3,15 @@ import pandas as pd
 import pytest
 import numpy as np
 import sys
+from astroquery.sdss import SDSS
 sys.path.append("app/")
 from data_extraction import Data
 from classification import Classifier
 
 # do we need a test of init too?
+
+query = "SELECT TOP 10 * FROM SpecObj"
+query_result = SDSS.query_sql(query)
 
 class TestFit:
     '''Test fit method of Classifier'''
@@ -22,6 +26,7 @@ class TestFit:
         y_large = np.random.choice(return_values, 4)
         y_wrong = np.random.rand(3)
         with pytest.raises(ValueError):
+            classifier.fit([],[])
             # x should be a 2d array
             classifier.fit(x_1d, y_correct)
             # x.shape[0] and len(y) should be the same
@@ -31,10 +36,10 @@ class TestFit:
 
     def test_fit_return_none(self):
         '''Tests that fit method returns None'''
-        return_values = {"GALAXY", "QSO", "STAR"}
+        data = pd.DataFrame(SDSS.get_spectra(matches=query_result)[0][1].data)
         classifier = Classifier()
-        x = np.random.rand(3,2)
-        y = np.random.choice(return_values, 3)
+        x = data.drop(columns='class')
+        y = data['class']
         assert classifier.fit(x,y) is None
 
 
@@ -54,30 +59,29 @@ class TestPredict:
             classifier.predict(x_new)
             # empty list input to predict
             classifier.predict([])
-            # wrong y values
-            classifier.fit(x,y_wrong)
 
     def test_predict_return_values(self):
         '''1d array of correct length is returned'''
-        return_values = {"GALAXY", "QSO", "STAR"}
+        data = pd.DataFrame(SDSS.get_spectra(matches=query_result)[0][1].data)
         classifier = Classifier()
-        x = np.random.rand(3,2)
-        y = np.random.choice(return_values, 3)
+        x = data.drop(columns='class')
+        y = data['class']
         classifier.fit(x,y)
-        x_new = np.random.rand(4,2)
-        assert classifier.predict(x_new).shape == (x_new.shape[0],)
+        assert classifier.predict(x).shape == (x.shape[0],)
 
     def test_predict_correct_values(self):
         '''Tests that the correct predictions are returned'''
         return_values = {"GALAXY", "QSO", "STAR"}
+        data = pd.DataFrame(SDSS.get_spectra(matches=query_result)[0][1].data)
+        classifier = Classifier()
+        x = data.drop(columns='class')
+        y = data['class']
         # create and fit classifier
         classifier = Classifier()
-        x = np.random.rand(3,2)
-        y = np.random.choice(return_values, 3)
         classifier.fit(x,y)
-        x_new = np.random.rand(4,2)
         # check that predictions are one of "GALAXY", "QSO", "STAR"
-        assert set(classifier.predict(x_new)).issubset(return_values)
+        assert set(classifier.predict(x)).issubset(return_values)
+
 
 class TestPredictProba:
     '''Test predict_proba method of Classifier'''
@@ -98,16 +102,15 @@ class TestPredictProba:
 
     def test_predict_proba_return_values(self):
         '''2d array is returned with 3 columns'''
-        return_values = {"GALAXY", "QSO", "STAR"}
+        data = pd.DataFrame(SDSS.get_spectra(matches=query_result)[0][1].data)
         classifier = Classifier()
-        x = np.random.rand(3,2)
-        y = np.random.choice(return_values, 3)
+        x = data.drop(columns='class')
+        y = data['class']
         classifier.fit(x,y)
-        x_new = np.random.rand(4,2)
         # returned array has correct shape
-        assert classifier.predict_proba(x_new).shape == (x_new.shape[0],3)
+        assert classifier.predict_proba(x).shape == (x.shape[0],3)
         # probabilities sum to 1 across rows
-        assert np.all(classifier.predict_proba(x_new).sum(axis=1),np.ones(x_new.shape[0]),atol=0.01)
+        assert np.all(classifier.predict_proba(x).sum(axis=1),np.ones(x.shape[0]),atol=0.01)
 
 
 class TestScore:
@@ -132,10 +135,10 @@ class TestScore:
 
     def test_score_return_value(self):
         '''Proper score is returned'''
-        return_values = {"GALAXY", "QSO", "STAR"}
+        data = pd.DataFrame(SDSS.get_spectra(matches=query_result)[0][1].data)
         classifier = Classifier()
-        x = np.random.rand(3,2)
-        y = np.random.choice(return_values, 3)
+        x = data.drop(columns='class')
+        y = data['class']
         classifier.fit(x,y)
         # should return a float:
         assert isinstance(classifier.score(x,y), float)
