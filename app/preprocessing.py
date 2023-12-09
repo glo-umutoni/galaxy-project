7190f68f150dc5936f60ede3b443883731ddc7eb
AlexandraRamassamy <alexandra_ramassamy@fas.harvard.edu>
2023-12-08 20:57:33 -0500

    feat: add preprocessing module skeleton

---
+++ app/preprocessing.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from typing import Tuple
+
+class Preprocessing:
+
+    @staticmethod
+    def normalize(data=pd.DataFrame):
+        pass
+
+    @staticmethod
+    def remove_outliers(data=pd.DataFrame):
+        pass
+
+    @staticmethod
+    def interpolate(x=list, y=list, range=Tuple[float, float], bin=int):
+        pass
+
+    @staticmethod
+    def correct_redshift(redshift = float, data=pd.DataFrame):
+        pass
