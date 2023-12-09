b58a671f5991ed8d791e39c1a44203606db8bb5f
Katherine Hunter <katherinehunter@g.harvard.edu>
2023-12-09 10:46:37 -0500

    test: modify response variable in classifier tests

--- test/test_classification.py
+++ test/test_classification.py
@@ -7,45 +7,46 @@ sys.path.append("app/")
 from data_extraction import Data
 from classification import Classifier
 
-# do I need to test the initialization?
+# do we need a test of init too?
 
 class TestFit:
     '''Test fit method of Classifier'''
 
     def test_fit_bad_inputs(self):
         '''Bad inputs are properly handled'''
+        return_values = {"GALAXY", "QSO", "STAR"}
         classifier = Classifier()
-        x_1d = np.random.rand(6)
+        x_1d = np.random.rand(3)
         x_2d = np.random.rand(3,2)
-        y_1d = np.random.rand(6)
-        y_2d = np.random.rand(3,2)
+        y_correct = np.random.choice(return_values, 3)
+        y_large = np.random.choice(return_values, 4)
+        y_wrong = np.random.rand(3)
         with pytest.raises(ValueError):
             # x should be a 2d array
-            classifier.fit(x_1d,y_1d)
+            classifier.fit(x_1d, y_correct)
             # x.shape[0] and len(y) should be the same
-            classifier.fit(x_2d, y_1d)
-            # y should be 1d array
-            classifier.fit(x_2d, y_2d)
+            classifier.fit(x_2d, y_large)
+            # y should contain "GALAXY", "QSO", "STAR"
+            classifier.fit(x_2d, y_wrong)
 
     def test_fit_return_none(self):
         '''Tests that fit method returns None'''
+        return_values = {"GALAXY", "QSO", "STAR"}
         classifier = Classifier()
         x = np.random.rand(3,2)
-        y = np.random.rand(3)
+        y = np.random.choice(return_values, 3)
         assert classifier.fit(x,y) is None
 
-    def test_fit_attribute_created(self):
-        '''Tests whatever we want to have inside new attribute'''
-        pass
 
 class TestPredict:
     '''Test predict method of Classifier'''
 
     def test_predict_bad_inputs(self):
         '''Bad inputs are properly handled'''
+        return_values = {"GALAXY", "QSO", "STAR"}
         classifier = Classifier()
         x = np.random.rand(3,2)
-        y = np.random.rand(3)
+        y = np.random.choice(return_values, 3)
         classifier.fit(x,y)
         x_new = np.random.rand(3,3)
         with pytest.raises(ValueError):
@@ -53,29 +54,40 @@ class TestPredict:
             classifier.predict(x_new)
             # empty list input to predict
             classifier.predict([])
+            # wrong y values
+            classifier.fit(x,y_wrong)
 
     def test_predict_return_values(self):
         '''1d array of correct length is returned'''
+        return_values = {"GALAXY", "QSO", "STAR"}
         classifier = Classifier()
         x = np.random.rand(3,2)
-        y = np.random.rand(3)
+        y = np.random.choice(return_values, 3)
         classifier.fit(x,y)
         x_new = np.random.rand(4,2)
         assert classifier.predict(x_new).shape == (x_new.shape[0],)
 
     def test_predict_correct_values(self):
         '''Tests that the correct predictions are returned'''
-        # what values should this return? are we using converting to -1, 0, 1?
-        pass
+        return_values = {"GALAXY", "QSO", "STAR"}
+        # create and fit classifier
+        classifier = Classifier()
+        x = np.random.rand(3,2)
+        y = np.random.choice(return_values, 3)
+        classifier.fit(x,y)
+        x_new = np.random.rand(4,2)
+        # check that predictions are one of "GALAXY", "QSO", "STAR"
+        assert set(classifier.predict(x_new)).issubset(return_values)
 
 class TestPredictProba:
     '''Test predict_proba method of Classifier'''
 
     def test_predict_proba_bad_inputs(self):
         '''Bad inputs are properly handled'''
+        return_values = {"GALAXY", "QSO", "STAR"}
         classifier = Classifier()
         x = np.random.rand(3,2)
-        y = np.random.rand(3)
+        y = np.random.choice(return_values, 3)
         classifier.fit(x,y)
         x_new = np.random.rand(3,3)
         with pytest.raises(ValueError):
@@ -86,28 +98,30 @@ class TestPredictProba:
 
     def test_predict_proba_return_values(self):
         '''2d array is returned with 3 columns'''
+        return_values = {"GALAXY", "QSO", "STAR"}
         classifier = Classifier()
         x = np.random.rand(3,2)
-        y = np.random.rand(3)
+        y = np.random.choice(return_values, 3)
         classifier.fit(x,y)
         x_new = np.random.rand(4,2)
+        # returned array has correct shape
         assert classifier.predict_proba(x_new).shape == (x_new.shape[0],3)
-    
-    def test_predict_proba_correct_values(self):
-        '''Test that correct values are returned'''
-        pass
+        # probabilities sum to 1 across rows
+        assert np.all(classifier.predict_proba(x_new).sum(axis=1),np.ones(x_new.shape[0]),atol=0.01)
+
 
 class TestScore:
     '''Test score method of Classifier'''
 
     def test_score_bad_inputs(self):
         '''Bad inputs are properly handled'''
+        return_values = {"GALAXY", "QSO", "STAR"}
         classifier = Classifier()
         x = np.random.rand(3,2)
-        y = np.random.rand(3)
+        y = np.random.choice(return_values, 3)
         classifier.fit(x,y)
         x_new = np.random.rand(4,2)
-        y_new = np.random.rand(3)
+        y_new = np.random.choice(return_values, 3)
         with pytest.raises(ValueError):
             # x_new and y have different lengths
             classifier.score(x_new, y)
@@ -118,16 +132,12 @@ class TestScore:
 
     def test_score_return_value(self):
         '''Proper score is returned'''
+        return_values = {"GALAXY", "QSO", "STAR"}
         classifier = Classifier()
         x = np.random.rand(3,2)
-        y = np.random.rand(3)
+        y = np.random.choice(return_values, 3)
         classifier.fit(x,y)
         # should return a float:
         assert isinstance(classifier.score(x,y), float)
         # should be a probability:
-        assert 0 <= classifier.score(x,y) <= classifier.score
-
-
-    def test_score_correct_value(self):
-        '''Returned score is correct'''
-        pass
+        assert 0 <= classifier.score(x,y) <= 1
