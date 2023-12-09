e4127a8c03a22ce2a72217c7988b2cca50f0740f
kay572 <kyan@g.harvard.edu>
2023-12-09 14:50:29 -0500

    test: change desired return type of plot func

--- test/test_visualization.py
+++ test/test_visualization.py
@@ -2,24 +2,17 @@ import os
 import sys
 from astroquery.sdss import SDSS
 import pandas as pd
+import matplotlib.figure
 import matplotlib.pyplot as plt
 import shutil
 import pytest
 sys.path.append("app/")
 from visualization import Visualization
 
-test_sandbox_directory_path = "test/test_env"
-
 # sample query and spectra data for testing purposes
 query = "SELECT TOP 10 * FROM SpecObj"
 query_result = SDSS.query_sql(query)
 data = pd.DataFrame(SDSS.get_spectra(matches=query_result)[0][1].data)
-
-@pytest.fixture(autouse=True)
-def clean_up():
-    os.makedirs(test_sandbox_directory_path, exist_ok=True)
-    yield
-    shutil.rmtree(test_sandbox_directory_path)
 
 class TestVisualization:
     '''Test the Visualization class from the visualization module'''
@@ -40,10 +33,8 @@ class TestVisualization:
 
     def test_plot(self):
         '''Test plot function when input is valid'''
-        # check that plot function with correct input creates figure that can be saved
+        # check that plot function with correct input runs
         output = Visualization.plot(data)
-        output_fig_path = os.path.join(test_sandbox_directory_path, "output.png")
-        plt.savefig(output_fig_path)
 
-        # check for correct return value
-        assert output is None
+        # check that function returns a matplotlib figure
+        assert isinstance(output, matplotlib.figure.Figure)
