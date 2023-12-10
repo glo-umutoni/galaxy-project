14b8e435745b5ec8a1e6b73c85745861d6c32d98
Glo Umutoni <gumutoni@g.harvard.edu>
2023-12-10 13:19:49 -0500

    Fix: edit the tests for alignment to account for Preprocessing housing the interpolate function

--- test/test_wavelength_alignment.py
+++ test/test_wavelength_alignment.py
@@ -1,33 +1,45 @@
 import os
 import shutil
 import pytest
+import numpy as np
 import sys
 sys.path.append("../app/")
 from wavelength_alignment import WavelengthAlignment
 
 class TestWavelengthAlignment:
     '''Test the WavelengthAlignment class from the wavelength_alignment module'''
-    def test_init(self):
-        wave_align= WavelengthAlignment()
-        assert wave_align.interpolated_x is None
-        assert type(wave_align.interpolated_y) == list
 
-    def test_interpolate(self):
-        object_id = 299489952322840576
-        min_val, max_val = 3.7, 3.8
-        num_points = 5000
-        wv = WavelengthAlignment()
-        linear_wavelengths, interpolated_flux=wv.interpolate(object_id=object_id, min_val=min_val, max_val=max_val, num_points=num_points)
-        assert len(interpolated_flux) == 5000
-        assert len(linear_wavelengths) ==5000
+    def test_interpolate_handle_bad_inputs(self):
+        '''Check that bad inputs raise the correct errors'''
+        incorrect_min_val, incorrect_max_val = 0, 0
+        incorrect_object_ids = (299489677444933632, 299489677444933632)
+        incorrect_pts = 312.2
 
-    def test_align(self):
-        object_ids = [299489952322840576, 299489677444933632]
-        min, max = 3.7, 3.8
-        num_points = 5000
-        wv = WavelengthAlignment()
-        wv.align(object_ids=object_ids, min_val=min, max_val=max, num_points=num_points)
-        inter_flux = wv.interpolated_y
-        inter_loglam = wv.interpolated_x
-        assert len(inter_flux) == 2
-        assert len(inter_loglam) == 5000
+        with pytest.raises(ValueError):
+            # incorrect entries for object ids
+            WavelengthAlignment.align(object_ids=incorrect_object_ids, min_val=3.5, max_val=3.7, num_points=500)
+            # incorrect minimum value
+            WavelengthAlignment.align(object_ids=[299489677444933632, 299489677444933632], min_val=incorrect_min_val
+                                      ,max_val=3.7, num_points=500)
+
+            # incorrect maximum value
+            WavelengthAlignment.align(object_ids=[299489677444933632, 299489677444933632], min_val=3.5
+                                      , max_val=incorrect_max_val, num_points=500)
+            # incorrect object ids
+            WavelengthAlignment.align(object_ids=[299489677444933632, 299489677444933632], min_val=3.5
+                                      , max_val=3.7, num_points=incorrect_pts)
+
+    def test_align_correct_values(self):
+        '''check handling of correct inputs'''
+        min_val = 3.2
+        max_val = 3.7
+        num_points = 500
+        object_ids = [299489677444933632, 299489677444933632]
+        aligned_x, aligned_y = WavelengthAlignment.align(object_ids=object_ids, min_val=min_val, max_val=max_val,
+                                                         num_points=num_points)
+        assert type(aligned_x) == np.ndarray
+        assert type(aligned_y) == list
+        assert len(aligned_x) == num_points
+        assert len(aligned_y) == len(object_ids)
+        assert min(aligned_x) >= min_val
+        assert max(aligned_x) <= max_val
