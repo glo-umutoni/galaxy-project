9cad1d584a96d3b8626a5f28da73daa7dbb1c703
AlexandraRamassamy <alexandra_ramassamy@fas.harvard.edu>
2023-12-11 14:53:26 -0500

    fix : fix wavelength_alignment to better handle Preprocessing.interpolate function

--- app/wavelength_alignment.py
+++ app/wavelength_alignment.py
@@ -1,5 +1,8 @@
 '''Module used to extract and/or combine astronomical data from Sloan Digital Sky Survey.'''
 import sys
+import pandas as pd
+import numpy as np
+from astroquery.sdss import SDSS
 sys.path.append("/app/")
 from preprocessing import Preprocessing
 
@@ -32,8 +35,17 @@ class WavelengthAlignment:
             raise ValueError("Range upper bound cannot be empty")
 
         aligned_y=[]
-        for object in (object_ids):
-            x, y = Preprocessing.interpolate(object_id=object, min_val=min_val, max_val=max_val, num_points=num_points)
+        for object_id in (object_ids):
+            object_id_str = str(object_id)
+            query = rf"SELECT * FROM SpecObj where specObjID in ({object_id_str})"
+            query_result = SDSS.query_sql(query)
+            # Retrieve spectral data: spectra[0] stores all the files related to the spectra for the object of interest and spectrum is stored as a table in the second item of the list.
+            spectra = SDSS.get_spectra(matches=query_result)
+            spectra_data = pd.DataFrame(spectra[0][1].data)
+            # Extract logarithm of wavelength and flux
+            loglam = np.array(spectra_data['loglam'], dtype=float)
+            flux = np.array(spectra_data['flux'], dtype=float)
+            x, y = Preprocessing.interpolate(x=loglam, y=flux, x_lim=(min_val, max_val), bins=num_points)
             aligned_x= x
             aligned_y.append(y)
         return aligned_x,aligned_y
