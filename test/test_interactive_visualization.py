0d0841296ad642bb56e84daa474e9aa2c0774cda
kay572 <kyan@g.harvard.edu>
2023-12-14 14:36:23 -0500

    test fix: check for correct return type in interactive vis unit

--- test/test_interactive_visualization.py
+++ test/test_interactive_visualization.py
@@ -4,11 +4,13 @@ from astroquery.sdss import SDSS
 import pandas as pd
 import numpy as np
 import matplotlib.figure
+import matplotlib.widgets
 import matplotlib.pyplot as plt
 import shutil
 import pytest
 sys.path.append("app/")
 from interactive_visualization import InteractiveVisualization
+matplotlib.use('Agg')
 
 # sample query and spectra data for testing purposes
 query = "SELECT TOP 10 * FROM SpecObj"
@@ -48,15 +50,16 @@ class TestInteractiveVisualization:
         # check that plot function with correct input runs
         output = InteractiveVisualization.plot(data, y_column="flux")
 
-        # check that function returns a matplotlib figure
-        assert output is None
+        # check that function returns a matplotlib.widgets.SpanSelector object
+        assert isinstance(output, matplotlib.widgets.SpanSelector)
 
         # check that custom keyword arguments work
         output = InteractiveVisualization.plot(data, y_column="flux", order=3)
 
-        # check that function returns a matplotlib figure
-        assert output is None
+        # check that function returns a matplotlib.widgets.SpanSelector object
+        assert isinstance(output, matplotlib.widgets.SpanSelector)
 
         fig, ax = plt.subplots()
         output = InteractiveVisualization.plot(data, y_column="flux", figax = (fig, ax), order=3)
-        assert output is None
+        # check that function returns a matplotlib.widgets.SpanSelector object
+        assert isinstance(output, matplotlib.widgets.SpanSelector)
